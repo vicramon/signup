027bf3ea241ba8a2af3aa3a5a36bbe5a500761b0
Vic Ramon <vic@vicramon.com>
2013-12-13 16:49:44 -0500

    Add users migration, model

---
+++ db/migrate/20131213214828_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :remember_token
+      t.string :reset_password_token
+      t.string :session_token
+
+      t.timestamps
+    end
+  end
+end
