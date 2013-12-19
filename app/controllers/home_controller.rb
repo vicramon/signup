07b0516f48609d21f4ff7dc5892d395c7a2ca4ca
Vic Ramon <vic@vicramon.com>
2013-12-19 18:07:06 -0500

    Setup basic sign in flow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,9 @@
 class HomeController < ApplicationController
-  expose(:user) { User.new }
+  before_filter :redirect_if_signed_in
+
+  private
+
+  def redirect_if_signed_in
+    redirect_to :dashboard if signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Signup::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :home, only: [:index]
   resources :dashboard, only: [:index]
-  resources :users, only: [:create]
+  resources :users, only: [:new, :create]
 
   get 'sign_out', to: 'sessions#destroy', as: 'sign_out'
-  get 'sign_in', to: 'home#index', as: 'sign_in'
+  get 'sign_in', to: 'sessions#new', as: 'sign_in'
+
+  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
 
   root to: 'home#index'
 end
