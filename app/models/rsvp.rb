d723894ae888223b67af7a6e0a8fd3a471ef1a29
Vic Ramon <vic@vicramon.com>
2014-01-14 18:17:43 -0500

    Properly show user field info data

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -3,4 +3,10 @@ class Rsvp < ActiveRecord::Base
   belongs_to :form
   belongs_to :slot
   has_many :field_infos
+
+  def info_for_field(field)
+    field_info = field_infos.where(field: field).first
+    field_info.info if field_info
+  end
+
 end
